17803776d9d8002e8aa7c35a7f0b076b9505af58
Yoseph birhane <yoseph7091@gmail.com>
2025-07-20 12:09:22 +0300

    Add Inline CSS Styling to React Components

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
 return (
-<main>
+<main >
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 );
--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 const UserProfile = (props) => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+    <div >
+      <h2 >{props.name}</h2>
+      <p>Age:  {props.age} </p>
       <p>Bio: {props.bio}</p>
     </div>
   );
