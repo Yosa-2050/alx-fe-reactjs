11a0f9dca3dcd5edeb13474d7430206794f8a308
Yoseph birhane <yoseph7091@gmail.com>
2025-07-27 17:44:56 +0300

    Added recipe action components: Delete, Edit, and Details

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,4 +6,14 @@ export const useRecipeStore = create((set) => ({
     recipes: [...state.recipes,newRecipe],
    })),
    setRecipes:(recipes) => set({ recipes}),
+   deleteRecipe: (id) => 
+      set((state) => ({
+         recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      })),
+   updateRecipe: (updateRecipe) =>
+      set((state) => ({
+        recipes: state.recipes.map((recipe) =>
+      recipe.id === updateRecipe.id ? updateRecipe : recipe
+      ),
+      })),
 }));
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,11 +1,18 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx'
 import './index.css'
+import RecipeDetails from './components/RecipeDetails.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Router>
+      <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
+    </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
