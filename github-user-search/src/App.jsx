a9902e33d9f843f45fd0c40a7303c1ef009ca9b4
Yoseph birhane <yoseph7091@gmail.com>
2025-08-03 01:07:35 +0300

    update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 function app () {
   return (
     <div>
       <h1>GitHub search application</h1>
       <p>setup successful</p>
     </div>
-  );
+  )
 }
 
 export default App;
