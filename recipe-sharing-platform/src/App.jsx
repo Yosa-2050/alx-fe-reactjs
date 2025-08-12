13f85d67db74d504ba63c59a142c28506ef4d78a
Yoseph birhane <yoseph7091@gmail.com>
2025-08-12 16:58:56 +0300

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import React from 'react'
+import './index.css'  // only import tailwind css here
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-green-900 text-2xl font-bold'>
+          go to hell brooo woooooo!!!!!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
